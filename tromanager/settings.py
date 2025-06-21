45123bb897a49575fc625c853a1a239bf83c34d5
Kis <thanhcong01052002@gmail.com>
2025-06-21 15:17:14 +0700

    Deploy CD to railway

--- tromanager/settings.py
+++ tromanager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-(r+r1j(yia5aiw-!mh4j@m1g9m01v)#g!78rir_$jv+vuq^-p_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
